989174198084d57c1f83568caf68cc7356d620b0
Jessica Lotto <jessica@example.com>
2025-12-09 13:03:05 -0500

    add assignment1

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -75,4 +75,53 @@ def grade (*args):
         # return f'Invalid data was provided.'
     except TypeError:
         return f'Invalid data was provided.'
-print(grade(75,85,95))
+# print(grade(75,85,95))
+
+# Task 6: Use a For Loop with a Range
+#
+def repeat(str, cnt):
+    
+    result = ''
+    for i in range(cnt):
+        result += str
+    return result
+        
+# print(repeat('hi',3))
+
+# Task 7: Student Scores, Using **kwargs
+
+def student_scores(mode, **kwargs):
+    if mode == "best":
+        best_name = ""
+        best_score = 0
+
+        for name, score in kwargs.items():
+            if score > best_score:
+                best_score = score
+                best_name = name
+
+        return best_name
+
+    elif mode == "mean":
+        total = 0
+        count = 0
+
+        for score in kwargs.values():
+            total += score
+            count += 1
+
+        return total / count
+# Task 8: Titleize, with String and List Operations
+# Create a function called titleize. It accepts one parameter, a string. The function returns a new string, where the parameter string is capitalized as if it were a book title.
+# The rules for title capitalization are: (1) The first word is always capitalized. (2) The last word is always capitalized. (3) All the other words are capitalized, except little words. For the purposes of this task, the little words are "a", "on", "an", "the", "of", "and", "is", and "in".
+# The following string methods may be helpful: split(), join(), and capitalize(). Look 'em up.
+# The split() method returns a list. You might store this in the words variable. words[-1] gives the last element in the list.
+# The in comparison operator: You have seen in used in loops. But it can also be used for comparisons, for example to check to see if a substring occurs in a string, or a value occurs in a list.
+# A new trick: As you loop through the words in the words list, it is helpful to have the index of the word for each iteration. You can access that index using the enumerate() function:
+# for i, word in enumerate(words):
+
+def titleize(str):
+    little_words = ["a", "on", "an", "the", "of", "and", "is","in"] 
+    for i, word in enumerate(str):
+        if word in little_words:
+            
